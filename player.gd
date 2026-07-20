76c0431d12c53ef6d201ca703e79d23605be1dc1
Adam Baranek <43297143+baranekadam76@users.noreply.github.com>
2026-07-19 22:50:49 -0400

    added basic animation

--- player.gd
+++ player.gd
@@ -6,4 +6,18 @@ var speed: int = 150
 func _physics_process(delta):
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	playerAnimationsAdam()
 	move_and_slide()
+
+func playerAnimationsAdam(): 
+	if direction:
+		if direction.x > 0:
+			$AnimatedSprite2D.animation = 'rightWalking'
+		if direction.x < 0:
+			$AnimatedSprite2D.animation = 'leftWalking'
+		if direction.y > 0:
+			$AnimatedSprite2D.animation = 'downWalking'
+		if direction.y < 0:
+			$AnimatedSprite2D.animation = 'upWalking'
+	else:
+		$AnimatedSprite2D.animation = 'idle'
