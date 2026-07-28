9173a10a975ac3ee829c1506edf4e3c37e08c60e
Adam Baranek <43297143+baranekadam76@users.noreply.github.com>
2026-07-28 15:15:57 -0400

    Smooth scene transitions
    Global scene for Transition has been created so that transitioning between scenes is smooth and also handled through the SceneManager.gd global script.

--- Scenes/Scripts/SceneManager.gd
+++ Scenes/Scripts/SceneManager.gd
@@ -2,7 +2,16 @@ extends Node
 
 var spawn_id : String = ""
 var player = null
+var is_transitioning := false
+
 
 func change_scene(scene_path : String, entrance : String):
+	if is_transitioning:
+		return
+	is_transitioning = true
 	spawn_id = entrance
+	await Transition.fade_out()
 	get_tree().change_scene_to_file(scene_path)
+	await get_tree().process_frame
+	await Transition.fade_in()
+	is_transitioning = false
